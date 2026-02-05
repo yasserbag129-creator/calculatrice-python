b879e464b68a93e58efbd8c31ed3881349b36de7
yasserbag129-creator <yasser.bag129@icloud.com>
2026-02-05 19:18:52 +0100

    Enhance calculator to support multiple operations
    Refactor calculator logic to allow multiple operations and user continuation.

--- calculatrice.py
+++ calculatrice.py
@@ -1,31 +1,44 @@
 print("Bienvenue dans la calculatrice :")
 
-a = float(input("Entrez un nombre :"))
+continuer = "o"
 
-operation = int(input("Entrez l'opération désirée (Tapez 1 pour addition, 2 pour soustraction, 3 pour multiplication, 4 pour la division) :"))
+while continuer == "o":
 
-b = float(input("Entrez le second nombre :"))
+    a = float(input("Entrez un nombre :"))
 
-if operation == 1:
-    print("Le résultat : ",a+b)
-
-elif operation == 2:
-    print("Le résultat : ",a-b)
-
-elif operation == 3:
-    print("Le résultat : ",a*b)
-
-elif operation == 4:
-
-    if b == 0:
+    print("1-Addition")
+    print("2-Soustraction")
+    print("3-Multiplication")
+    print("4-Division")
+    operation = int(input("Entrez l'opération désirée :"))
     
+
+    b = float(input("Entrez le second nombre :"))
+
+    if operation == 1:
+        print("Le résultat : ",a+b)
+
+    elif operation == 2:
+        print("Le résultat : ",a-b)
+
+    elif operation == 3:
+        print("Le résultat : ",a*b)
+
+    elif operation == 4:
+
         while b == 0:
             print("L'opération n'est pas possible.")
             b = float(input("Entrez le second nombre :"))
         
         print("Le résultat : ",a/b)
+    
     else:
-        print("Le résultat : ",a/b)
-
-else:
-    print("Ce n'est pas une opération valide.")
+        print("Ce n'est pas une opération valide.")
+    
+    print("Voilà !")
+    continuer = input("Veux-tu continuer ? (o/n) :")
+    if continuer == "o":
+        print("On continue !")
+    else:
+        print("Au revoir !")
+        break
